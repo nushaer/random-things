ef69de182236c911ed55a336095a246487826d18
nushaer <143752243+nushaer@users.noreply.github.com>
2024-02-06 23:28:24 +0600

    Add files via upload
    A simple python code that takes input from the user and solves a set of simultaneous equations of two variables

---
+++ twovar_solver.py
@@ -0,0 +1,13 @@
+def twotwosolver (a1,b1,c1,a2,b2,c2):
+    x = (b1*c2-b2*c1)/(a1*b2-a2*b1)
+    y = (-c1-a1*x)/b1
+    return [x,y]
+
+print('Enter the co-efficients for a set of simultaneous equations. \n Remember to use the form ax + by + c = 0 ')
+
+A1,B1,C1 = [int(x) for x in input("enter the coefficients for the first equation\n").split()]
+A2,B2,C2 = [int(x) for x in input("enter the coefficients for the second equation\n").split()]
+
+#[x_solve,y_solve] = twotwosolver(A1,B1,C1,A2,B2,C2)
+
+print('The solution to \n',A1,'x + ',B1,'y + ',C1,' = 0 \n ',A2,'x + ',B2,'y + ',C2,' = 0 is: \n ',twotwosolver(A1,B1,C1,A2,B2,C2))
